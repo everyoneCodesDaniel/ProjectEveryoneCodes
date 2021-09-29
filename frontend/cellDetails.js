a2cf1e8d5de316f43c7e3c61e6c1177c5055b321
everyoneCodesDaniel <87306750+everyoneCodesDaniel@users.noreply.github.com>
2021-09-29 11:36:31 +0200

    Update cellDetails.js
    Add function!

--- frontend/cellDetails.js
+++ frontend/cellDetails.js
@@ -1,3 +1,11 @@
 function checkCellPath() {
     
 }
+
+function functionone() {
+
+    var chatWindow = document.getElementById("chatWindow");
+    chatWindow.innerHTML = "<p><b>" +
+        "<i> " + username + ": </i></b>" +
+        "Hallo!!!!!" + "</p>" + chatWindow.innerHTML;
+};
