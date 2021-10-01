95e772328657a0a2a3a96eedb6248125aaa5e0c4
AymaraCE <aymara.cortez@gmail.com>
2021-10-01 17:08:35 +0200

    Version 3

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const dbconfig = require('./dbconfig.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const session = require('express-session');
-
+const { count } = require('console');
 
 // parse HTTP POST Data 
 app.use(express.urlencoded({extended: true})); 
@@ -15,13 +15,13 @@ app.use(express.json()); // accept json data
 // put client-side code (html/css/js) in the frontend folder
 app.use(express.static(__dirname + '/../frontend'));
 
+// User login status
 app.use(session({
     secret: 'keyboard cat', 
     cookie: { secure: true},
     resave: true,
     saveUninitialized: true}
 ));
-
 
 connection = mysql.createConnection(dbconfig.dbSettings);
 connection.connect((err) => {
@@ -88,6 +88,171 @@ app.post("/project/loginplayer", async (req, res) => {
         }
     });
 });
+
+var counter = 0;
+
+app.put("/startgame/:username", async (req, res) => {
+    await dbQuery("create table durchläufe" + counter + "(id int primary key auto_increment," +
+        "ergebnis VARCHAR(10) DEFAULT NULL check (ergebnis in ('gewonnen', 'verloren'))," +
+        "zuganzahl int DEFAULT 0,personId int not NULL," +
+        "constraint durchlauf" + counter + " foreign key (personId) REFERENCES personen (personId));"
+    );
+    let id;
+    await dbQuery("SELECT personId FROM personen where username=?", [req.params.username]).then(async rows => {
+        id = rows[0].personId
+        await dbQuery("insert into durchläufe" + counter + "(personId) values (?);",
+            [id]).then(async rows => {
+                console.log(rows)
+                await dbQuery("create table details" + counter + "(zuganzahl int primary key auto_increment," +
+                    "look VARCHAR(1) DEFAULT NULL,pickup VARCHAR(1) DEFAULT NULL," +
+                    "apply VARCHAR(1) DEFAULT NULL,talk VARCHAR(1) DEFAULT NULL," +
+                    "richtung VARCHAR (10) DEFAULT NULL,position VARCHAR(20) not NULL," +
+                    "durchlaufid int not NULL," +
+                    "constraint details" + counter + " foreign key (durchlaufid) REFERENCES durchläufe" + counter + "(id));").then(
+                        connection.query("insert into details" + counter++ + " (position,durchlaufid) values ('x3/y3',1)")
+                    )
+            })
+    })
+});
+
+app.put("/exitgame", async (req, res) => {
+    console.log("exitOida!");
+    var zug;
+    var id;
+    var counter2 = counter-1;
+    console.log(counter2)
+    await dbQuery("SELECT zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        console.log(rows)
+        if (rows.length === 1) {
+            zug = 0;
+        } else {
+            zug = rows[0].zuganzahl
+        }
+        console.log(zug)
+        await dbQuery("select id FROM durchläufe" + counter2 + " ORDER BY id DESC LIMIT 1;").then(rows => {
+                id = rows[0].id
+                console.log(id)    
+                connection.query("update durchläufe" + counter2 + " set zuganzahl = " + zug + ", ergebnis = 'verloren' where id = " + id);
+            });
+        });
+    });
+
+app.put("/wongame", async (req, res) => {
+    console.log("wonOida!");
+    var zug;
+    var id;
+    var counter2 = counter-1;
+    console.log(counter2)
+    await dbQuery("SELECT zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        console.log(rows)
+        if (rows.length === 1) {
+            zug = 0;
+        } else {
+            zug = rows[0].zuganzahl
+        }
+        console.log(zug)
+        await dbQuery("select id FROM durchläufe" + counter2 + " ORDER BY id DESC LIMIT 1;").then(rows => {
+            id = rows[0].id
+            console.log(id)
+            connection.query("update durchläufe" + counter2 + " set zuganzahl = " + zug + ", ergebnis = 'gewonnen' where id = " + id);
+        });
+    });
+});
+
+app.put("/direction/north", async (req, res) => {
+    var id;
+    console.log('idvorhernorth ' + id)
+    var x = req.body.x;
+    console.log('xvorhernorth ' +x)
+    var y = req.body.y;
+    console.log('yvorhernorth ' + y)
+    var counter2 = counter-1;
+    console.log('c2vorhernorth ' + counter2)
+    console.log('cvorhernorth ' + counter)
+    await dbQuery("select zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        id = rows[0].zuganzahl
+        await dbQuery("update details" + counter2 + " set richtung = 'n' where zuganzahl = " + id).then(
+            connection.query("insert into details" + counter2 + " (position,durchlaufid) values ('x" + x + "/y" + y + "',1)")
+        );
+    });
+    console.log('idnachhernorth ' + id)
+    console.log('xnachhernorth ' + x)
+    console.log('ynachhernorth ' + y)
+    console.log('c2nachhernorth ' + counter2)
+    console.log('cnachhernorth ' + counter)
+});
+
+app.put("/direction/south", async (req, res) => {
+    var id;
+    console.log('idvorhersouth ' + id)
+    var x = req.body.x;
+    console.log('xvorhersouth ' + x)
+    var y = req.body.y;
+    console.log('yvorhersouth ' + y)
+    var counter2 = counter-1;
+    console.log('c2vorhersouth ' + counter2)
+    console.log('cvorhersouth ' + counter)
+    await dbQuery("select zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        id = rows[0].zuganzahl
+        await dbQuery("update details" + counter2 + " set richtung = 's' where zuganzahl = " + id).then(
+            connection.query("insert into details" + counter2 + " (position,durchlaufid) values ('x" + x + "/y" + y + "',1)")
+        );
+    });
+    console.log('idnachhersouth ' + id)
+    console.log('xnachhersouth ' + x)
+    console.log('ynachhersouth ' + y)
+    console.log('c2nachhersouth ' + counter2)
+    console.log('cnachhersouth ' + counter)
+});
+
+app.put("/direction/east", async (req, res) => {
+    var id;
+    console.log('idvorhereast ' + id)
+    var x = req.body.x;
+    console.log('xvorhereast ' + x)
+    var y = req.body.y;
+    console.log('yvorhereast ' + y)
+    var counter2 = counter-1;
+    console.log('c2vorhereast ' + counter2)
+    console.log('cvorhereast ' + counter)
+    await dbQuery("select zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        id = rows[0].zuganzahl
+        await dbQuery("update details" + counter2 + " set richtung = 'e' where zuganzahl = " + id).then(
+            connection.query("insert into details" + counter2 + " (position,durchlaufid) values ('x" + x + "/y" + y + "',1)")
+        );
+    });
+    console.log('idnachhereast ' + id)
+    console.log('xnachhereast ' + x)
+    console.log('ynachhereast ' + y)
+    console.log('c2nachhereast ' + counter2)
+    console.log('cnachhereast ' + counter)
+});
+
+app.put("/direction/west", async (req, res) => {
+    var id;
+    console.log('idvorherwest ' + id)
+    var x = req.body.x;
+    console.log('xvorherwest ' + x)
+    var y = req.body.y;
+    console.log('yvorherwest ' + y)
+    var counter2 = counter-1;
+    console.log('c2vorherwest ' + counter2)
+    console.log('cvorherwest ' + counter)
+    await dbQuery("select zuganzahl FROM details" + counter2 + " ORDER BY zuganzahl DESC LIMIT 1;").then(async rows => {
+        id = rows[0].zuganzahl
+        await dbQuery("update details" + counter2 + " set richtung = 'w' where zuganzahl = " + id).then(
+            connection.query("insert into details" + counter2 + " (position,durchlaufid) values ('x" + x + "/y" + y + "',1)")
+        );
+    });
+    console.log('idnachherwest ' + id)
+    console.log('xnachherwest ' + x)
+    console.log('ynachherwest ' + y)
+    console.log('c2nachherwest ' + counter2)
+    console.log('cnachherwest ' + counter)
+});
+
+/* let id = connection.query("select * from durchläufe where personId = ? order by id desc limit 1",
+[req.session.user.id]); */
 
 app.get("/logout", (req, res) => {
     req.session.destroy();
